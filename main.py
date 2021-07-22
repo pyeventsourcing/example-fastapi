255af1370ad360a783e76fafd5cef4338056902a
John Bywater <john.bywater@appropriatesoftware.net>
2021-07-22 16:39:30 +0100

    Reformatted main.py with Black.

--- main.py
+++ main.py
@@ -91,5 +91,5 @@ async def close_account(account_id: UUID):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=5000,
-                log_level="info", reload=True)
+
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info", reload=True)
